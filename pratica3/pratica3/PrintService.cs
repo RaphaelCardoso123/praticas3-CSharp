d4f892061b5009559163fd2dfea63fe081641226
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-07 08:52:00 -0300

    terceiro projeto

---
+++ pratica3/pratica3/PrintService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pratica3
+{
+    class PrintService
+    {
+    }
+}
---
+++ pratica3/pratica3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pratica3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
