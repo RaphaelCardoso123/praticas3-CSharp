148c35838ead9c95a69b3198824952a2be6b8adb
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-08 21:37:01 -0300

    Restrições de generics

---
+++ pratica3/Pratica4/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pratica4.Entities
+{
+    class Product
+    {
+    }
+}
---
+++ pratica3/Pratica4/Services/CalculationService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pratica4.Services
+{
+    class CalculationService
+    {
+    }
+}
