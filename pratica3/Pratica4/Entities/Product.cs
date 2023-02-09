de081230bdf279be2be4de20d847ccd70de060cd
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-08 22:43:34 -0300

    IComparable

--- pratica3/Pratica4/Entities/Product.cs
+++ pratica3/Pratica4/Entities/Product.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace Pratica4.Entities
 {
-    class Product
+    class Product : IComparable
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InstalledUICulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparing error: argument is not a Product");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
     }
 }
