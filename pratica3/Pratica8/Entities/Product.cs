d850194ab250d93c7446879a791fb86c7bc84786
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-10 15:37:28 -0300

    Predicate / RemoveAll

--- pratica3/Pratica8/Entities/Product.cs
+++ pratica3/Pratica8/Entities/Product.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Pratica8.Entities;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Pratica8.Entities
 {
     class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2");
+        }
     }
 }
--- pratica3/Pratica8/Program.cs
+++ pratica3/Pratica8/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using Pratica8.Entities;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Pratica8
 {
@@ -8,6 +8,19 @@ namespace Pratica8
     {
         static void Main(string[] args)
         {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+
+            list.RemoveAll(p => p.Price >= 100.00);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+
+                Console.ReadLine();
+            }
         }
     }
 }
