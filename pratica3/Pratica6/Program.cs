71cb5064e0083dbff96851e6ed9cc75e3e613e97
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-10 13:27:47 -0300

    prática usando Dictionary

---
+++ pratica3/Pratica6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pratica6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "Flávia";
+            cookies["email"] = "flagia@gmail";
+            cookies["phone"] = "991352372";
+
+            Console.WriteLine(cookies["user"]);
+
+            Console.ReadLine();
+        }
+    }
+    
+}
