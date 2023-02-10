0f81931ca46f006ea5e8e5be7b08af1bd900d535
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-10 14:22:34 -0300

    DateTime

---
+++ pratica3/Pratica7/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Pratica7.Extensions
+{
+    class DateTimeExtensions
+    {
+        static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + "Hours";
+            }
+            else
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + "Days";
+            }
+        }
+    }
+}
---
+++ pratica3/Pratica7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pratica7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023, 11, 15, 8, 10, 40);
+            Console.WriteLine(dt.ElapsedTime());
+
+            Console.ReadLine();
+        }
+    }
+}
