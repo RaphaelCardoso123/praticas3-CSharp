9aabc45518e9a83a549e2169e6b0f022e287176e
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-11 18:47:48 -0300

    Linq

---
+++ pratica3/Pratica10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Pratica10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //specify the date source
+            int[] numbers = new int[] { 2, 3, 4, 5, 6 };
+
+            //define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //execute the query
+            foreach(int x in result)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
