11a8fd9226b3baad1cd3787c69f8fa8aaba1ae38
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-12 11:23:07 -0300

    LINQ / Lambda

--- pratica3/Pratica11/Entities/Category.cs
+++ pratica3/Pratica11/Entities/Category.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace Pratica11.Entities
 {
     class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tier { get; set; }
     }
 }
