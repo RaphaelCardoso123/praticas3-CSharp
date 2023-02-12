ecb8e8ba2617798c737f7c1b8bb87eb81b221945
Raphael  Cardoso <cardoso.raphael@hotmail.com>
2023-02-12 10:57:56 -0300

    LINQ / Lambda

--- pratica3/Pratica11/Entities/Product.cs
+++ pratica3/Pratica11/Entities/Product.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Globalization;
 
 namespace Pratica11.Entities
 {
     class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;
+        }
     }
 }
