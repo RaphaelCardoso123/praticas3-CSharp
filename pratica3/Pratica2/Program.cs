7943dbec552a26b0eb057911f6b8a2991e94b2ef
Raphael Cardoso <cardoso.raphael@hotmail.com>
2023-02-16 15:52:36 -0300

    prática

--- pratica3/Pratica2/Program.cs
+++ pratica3/Pratica2/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using Pratica2.Entities;
+using System.IO;
+using System.Globalization;
 
 namespace Pratica2
 {
@@ -8,6 +12,19 @@ namespace Pratica2
         {
             Console.WriteLine("Enter full file path: ");
             string path = Console.ReadLine();
+
+            List<Product> list = new List<Product>();
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
